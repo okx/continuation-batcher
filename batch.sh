155d9d8e95779fce871f2cf2b37f7c0df77b5f4b
xingqiang.yuan <xingqiang.yuan@okg.com>
2023-08-29 17:41:47 +0800

    update

---
+++ batch.sh
@@ -0,0 +1,20 @@
+cargo run --release -- --output ./output batch --challenge poseidon -k 22 --info ../zkWasm/output/zkwasm.loadinfo.json --info ../zkWasm-host-circuits/output/POSEIDONHASH.loadinfo.json --name batchsample --commits sample/commits1.json
+
+#{
+#  "equivalents": [
+#    {
+#      "source": {"name": "POSEIDONHASH", "proof_idx": 0, "column_name": "shared_operands"},
+#      "target": {"name": "zkwasm", "proof_idx": 0, "column_name": "shared_operands"}
+#    },
+#    {
+#      "source": {"name": "MERKLE", "proof_idx": 0, "column_name": "shared_operands"},
+#      "target": {"name": "zkwasm", "proof_idx": 0, "column_name": "shared_operands"}
+#    },
+#    {
+#      "source": {"name": "JUBJUBSUM", "proof_idx": 0, "column_name": "shared_operands"},
+#      "target": {"name": "zkwasm", "proof_idx": 0, "column_name": "shared_operands"}
+#    }
+#  ],
+#  "expose": [],
+#  "absorb": []
+#}
