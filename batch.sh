b729075f3f5fc6c344473ffd71b5d042748a6d44
xingqiang.yuan <xingqiang.yuan@okg.com>
2023-08-29 17:48:59 +0800

    update

--- batch.sh
+++ batch.sh
@@ -1,4 +1,4 @@
-cargo run --release -- --output ./output batch --challenge poseidon -k 22 --info ../zkWasm/output/zkwasm.loadinfo.json --info ../zkWasm-host-circuits/output/POSEIDONHASH.loadinfo.json --name batchsample --commits sample/commits1.json
+cargo run --release -- --output ./output batch --challenge poseidon -k 22 --info output/zkwasm.loadinfo.json --info output/POSEIDONHASH.loadinfo.json --name batchsample --commits sample/commits1.json
 
 #{
 #  "equivalents": [
---
+++ prepare.sh
@@ -0,0 +1,5 @@
+cp ../zkWasm/output/*.json output
+cp ../zkWasm-host-circuits/output/*.json output
+
+cp ../zkWasm/output/*.data output
+cp ../zkWasm-host-circuits/output/*.data output
