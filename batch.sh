89d9288501ef871adfbf462ac2570c867799eb91
xingqiang.yuan <xingqiang.yuan@okg.com>
2023-09-06 15:36:06 +0800

    update

--- batch.sh
+++ batch.sh
@@ -1,1 +1,1 @@
-cargo run --release -- --output ./output --param ./output batch --challenge poseidon -k 22 --info output/block0.loadinfo.json output/block0.POSEIDONHASH.loadinfo.json output/block0.MERKLE.loadinfo.json output/block0.JUBJUBSUM.loadinfo.json --name batch0 --commits sample/batch0.json
+cargo run --release -- --output ./output --param ./output batch --challenge poseidon -k 23 --info output/block0.loadinfo.json output/block0.POSEIDONHASH.loadinfo.json output/block0.MERKLE.loadinfo.json output/block0.JUBJUBSUM.loadinfo.json --name batch0 --commits sample/batch0.json
