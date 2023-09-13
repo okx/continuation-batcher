f0ee72a7bebdf6260851aa8e85b1a5a64d33bf0d
xingqiang.yuan <xingqiang.yuan@okg.com>
2023-09-13 09:54:26 +0800

    debug

---
+++ batch6.sh
@@ -0,0 +1,1 @@
+cargo run --release --features cuda -- --output ./output --param ./output batch --challenge sha -k 23 --info output/final6.loadinfo.json output/batch6.loadinfo.json output/final6.POSEIDONHASH.loadinfo.json output/final6.MERKLE.loadinfo.json output/final6.JUBJUBSUM.loadinfo.json --name final_batch6 --commits sample/batch6.json
